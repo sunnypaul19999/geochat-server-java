13833f23385cbf615895f68875b9f8fa01e08fbd
sunnypaul19999 <sunnypaul19999@gmail.com>
2022-07-05 23:41:17 +0530

    added sequence generators for topic, subtopic and meta discussion

--- setup_schema/generators-schema.sql
+++ setup_schema/generators-schema.sql
@@ -2,4 +2,16 @@ create table geouser_id_seqgenerator(
 next_val bigint
 )engine = MyISAM;
 
-insert into geouser_id_seqgenerator (next_val) values(1)
+create table sub_topic_id_seqgenerator(
+next_val bigint
+)engine = MyISAM;
+
+create table sub_topic_meta_discuss_id_seqgenerator(
+next_val bigint
+)engine = MyISAM;
+
+insert into geouser_id_seqgenerator (next_val) values(1);
+
+insert into sub_topic_id_seqgenerator (next_val) values(1);
+
+insert into sub_topic_meta_discuss_id_seqgenerator (next_val) values(1);
